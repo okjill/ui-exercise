6402daa463660a08042d2825d99f247efeb16923
Jill Moore <okjill@gmail.com>
2020-06-03 05:30:29 -0500

    Updates

---
+++ precache-manifest.9ca738f8e53a91a98f5c92a1cdcc6b6a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c124fe08883dacb67aa016cee36e2d7",
+    "url": "/ui-exercise/index.html"
+  },
+  {
+    "revision": "13813694ae9ad78e1419",
+    "url": "/ui-exercise/static/css/main.8e666822.chunk.css"
+  },
+  {
+    "revision": "bfb4414488731c877dc7",
+    "url": "/ui-exercise/static/js/2.71788e69.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ui-exercise/static/js/2.71788e69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13813694ae9ad78e1419",
+    "url": "/ui-exercise/static/js/main.f29f8480.chunk.js"
+  },
+  {
+    "revision": "56ca0ed0144e8ed17ac8",
+    "url": "/ui-exercise/static/js/runtime-main.3ce0e33a.js"
+  },
+  {
+    "revision": "7f317c257b2ee4a18571c65c7a62ba29",
+    "url": "/ui-exercise/static/media/check.7f317c25.png"
+  },
+  {
+    "revision": "4ba3a84a55c4eecac49a494768634818",
+    "url": "/ui-exercise/static/media/debit.4ba3a84a.png"
+  }
+]);
